7df41c7e8a44f768e850145856b859f018297c43
ers <ersleeer@163.com>
2023-06-28 01:32:10 +0800

    common

---
+++ include/fitter.h
@@ -0,0 +1,57 @@
+﻿#ifndef _FITTER_H_
+#define _FITTER_H_
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+
+static const int UNSELECTED = 0;
+static const int INITED = 0;
+static const int ERASED = 1;
+
+
+struct KeyMap {
+    const char ESC = 0x1B;
+    const char U = 0x75;
+    const char UP = 0x77;
+    const char LEFT = 0x61;
+    const char DOWN = 0x73;
+    const char RIGHT = 0x64;
+    const char ENTER = 0x0D;
+};
+
+using point_t = struct point_t {
+    int x;
+    int y;
+};
+
+using p_with_v = struct p_with_v {
+    int value;
+    int state;
+};
+
+class Frame;
+class Key {
+    Frame *view;
+    point_t settedP;
+    int preVal;
+    int curVal;
+  public:
+    Key(Frame *pview) : view(pview) {};
+    Key(Frame *pview, const point_t &point, int preValue, int curValue);
+    Key(const Key &);
+    ~Key(){};
+
+    bool execute(int nInputValue);
+    void undo();
+    point_t getPoint() { return settedP; }
+    int getPreValue() { return preVal; }
+    int getCurValue() { return curVal; }
+    void setPoint(const point_t &point) { settedP = point; }
+    void setPreValue(int preValue) { preVal = preValue; }
+    void setCurValue(int curValue) { curVal = curValue; }
+};
+
+
+#endif
---
+++ src/fitter.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <cassert>
+#include <cmath>
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory.h>
+#include <unordered_map>
+#include <vector>
+#include "fitter.h"
+
+Key::Key(Frame *pview, const point_t &point, int preValue, int curValue)
+    : view(pview), settedP(point), preVal(preValue), curVal(curValue) {}
+
+Key::Key(const Key &key)
+    : view(key.view), settedP(key.settedP), preVal(key.preVal),
+      curVal(key.curVal) {}
+
+bool Key::execute(int nInputValue) {
+    if (!view)
+        return false;
+
+    settedP = view->getCurPoint();
+    return view->setCurValue(nInputValue, preVal);
+}
+
+void Key::undo() {
+    if (view) {
+        view->setPointValue(settedP, preVal);
+    }
+    return;
+}
